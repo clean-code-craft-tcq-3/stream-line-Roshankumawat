7b83ef134c31e10884bda325db9e8cf8eb654e69
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-07 16:13:07 +0530

    Create Sender_test.py

---
+++ Sender_test.py
@@ -0,0 +1,13 @@
+import Sender
+
+
+def test_generate_samples(min_value, max_value, no_of_samples):
+  # Expected no of samples and detected no of samples shall be same
+  samples= Sender.generate_samples(min_value, max_value, no_of_samples)
+  samples_length= len(samples)
+  assert(samples_length==no_of_samples)
+  
+  
+if __name__ == '__main__':
+  test_generate_samples(0, 40, 20)
+  
