2eb41c82bf95368cbe9d6cc2901fa572139b471e
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-10 19:10:12 +0530

    Update Sender_test.py

--- Sender_test.py
+++ Sender_test.py
@@ -3,23 +3,26 @@ import unittest
 
 class sender_test(unittest.TestCase):
   
-  def test_generate_samples(self, min_value, max_value, no_of_samples):
+  def test_generate_samples(self):
     # Expected no of samples and detected no of samples shall be same
-    samples= Sender.generate_samples(self.min_value, self.max_value, self.no_of_samples)
+    expected_samples= 30
+    samples= Sender.generate_samples(10,50, 30)
     samples_length= len(samples)
-    assert(samples_length==no_of_samples)
+    assert(samples_length==expected_samples)
   
   def test_celcious_to_farenheit_convertor(self):
     self.assertTrue(Sender.celcious_to_farenheit_convertor(50)==122)
   
   def test_get_temperature_in_C(self):
-    self.assertTrue(Sender.get_temperature_in_C(10, 50, 50) in range (10, 50))
+    samples= Sender.get_temperature_in_C(10, 50, 50)
+    for sample in samples:
+        self.assertTrue(sample in range (10, 50))
   
   def test_get_charge_rate(self):
-    self.assertTrue(Sender.get_charge_rate(0, 50, 50) in range(0, 50)) 
-  
-
+    samples= Sender.get_charge_rate(0, 50, 50)
+    for sample in samples:
+        self.assertTrue(sample in range (0, 50))
  
-  
+ 
 if __name__ == '__main__':
   unittest.main()
