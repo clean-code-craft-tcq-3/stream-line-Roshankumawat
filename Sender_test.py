4a5564ad98a7c696b04890eff3f315c07db3049a
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-10 18:40:44 +0530

    Update Sender_test.py

--- Sender_test.py
+++ Sender_test.py
@@ -1,26 +1,25 @@
 import Sender
+import unittest
 
+class sender_test(unittest.TestCase):
+  
+  def test_generate_samples(self, min_value, max_value, no_of_samples):
+    # Expected no of samples and detected no of samples shall be same
+    samples= Sender.generate_samples(self.min_value, self.max_value, self.no_of_samples)
+    samples_length= len(samples)
+    assert(samples_length==no_of_samples)
+  
+  def test_celcious_to_farenheit_convertor(self):
+    self.assertTrue(Sender.celcious_to_farenheit_convertor(50)==122)
+  
+  def test_get_temperature_in_C():
+    self.assertTrue(Sender.get_temperature_in_C(10, 50, 50) in range (10, 50))
+  
+  def test_get_charge_rate():
+    self.assertTrue(Sender.get_charge_rate(0, 50, 50) in range(0, 50)) 
+  
 
-def test_generate_samples(min_value, max_value, no_of_samples):
-  # Expected no of samples and detected no of samples shall be same
-  samples= Sender.generate_samples(min_value, max_value, no_of_samples)
-  samples_length= len(samples)
-  assert(samples_length==no_of_samples)
-  
-def test_celcious_to_farenheit_convertor():
-  assert(Sender.celcious_to_farenheit_convertor(50)==122)
-  
-def test_get_temperature_in_C():
-  assert(Sender.get_temperature_in_C(10, 50, 50) in range (10, 50))
-  
-def test_get_charge_rate():
-  assert(Sender.get_charge_rate(0, 50, 50) in range(0, 50)) 
  
   
 if __name__ == '__main__':
-  test_generate_samples(0, 40, 20)
-  test_generate_samples(-10, 40, 40)
-  test_get_charge_rate()
-  test_get_temperature_in_C()
-  assert(Sender.formate_message('Temperature', [10,20,30])=='The Temperature readings- [10, 20, 30]')
-  assert(Sender.display_readings('The Temperature readings- [10, 20, 30]')=='The Temperature readings- [10, 20, 30]')
+  unittest.main()
