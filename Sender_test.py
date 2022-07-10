f94597f2e0b13e0ed039605a2ace9898ce350fcd
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-10 18:12:14 +0530

    Update Sender_test.py

--- Sender_test.py
+++ Sender_test.py
@@ -7,6 +7,14 @@ def test_generate_samples(min_value, max_value, no_of_samples):
   samples_length= len(samples)
   assert(samples_length==no_of_samples)
   
+def test_celcious_to_farenheit_convertor(self):
+  self.assertTrue(sender.celcious_to_farenheit_convertor(50)==122)
+  
+def test_get_temperature_in_C(self):
+  self.assertTrue(sender.get_temperature_in_C(10, 50, 50) in range (10, 50))
+  
+def test_get_charge_rate(self):
+  self.assertTrue(sender.get_charge_rate(0, 50, 50) in range(0, 50)) 
  
   
 if __name__ == '__main__':
