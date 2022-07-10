a89817af58541205a8ba5a2c25b5b284a6e5ec9f
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-10 18:47:01 +0530

    Update Sender_test.py

--- Sender_test.py
+++ Sender_test.py
@@ -12,10 +12,10 @@ class sender_test(unittest.TestCase):
   def test_celcious_to_farenheit_convertor(self):
     self.assertTrue(Sender.celcious_to_farenheit_convertor(50)==122)
   
-  def test_get_temperature_in_C():
+  def test_get_temperature_in_C(self):
     self.assertTrue(Sender.get_temperature_in_C(10, 50, 50) in range (10, 50))
   
-  def test_get_charge_rate():
+  def test_get_charge_rate(self):
     self.assertTrue(Sender.get_charge_rate(0, 50, 50) in range(0, 50)) 
   
 
