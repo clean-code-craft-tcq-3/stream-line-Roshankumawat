96ccad2671e6570f592ee4e4b21d567228be922d
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-19 21:13:27 +0530

    Update Sender.py

--- Sender.py
+++ Sender.py
@@ -2,16 +2,16 @@ import random
 import json
 
 def get_temperature_in_C():
-    return generate_samples(0, 60, 50)
+    return generate_samples(0, 60)
 
 def celcious_to_farenheit_convertor(temp):
     return round((temp * 1.8) + 32,2)
      
-def generate_samples(min_value,max_value,no_of_samples):
-    return random.sample(range(min_value, max_value),no_of_samples)
+def generate_samples(min_value,max_value):
+    return random.randint(min_value, max_value)
   
 def get_charge_rate():
-    return generate_samples(20,80, 50)
+    return generate_samples(20,80)
 
 def process_data(temp_in_c, charge_rate):
     data= {}
