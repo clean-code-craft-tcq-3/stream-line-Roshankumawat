ee62bae8f6496bbfc40faf1f15e18a97a2cd4909
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-19 20:54:53 +0530

    Update Sender.py

--- Sender.py
+++ Sender.py
@@ -1,8 +1,8 @@
 import random 
 import json
 
-def get_temperature_in_C(min_value,max_value,no_of_samples):
-    return generate_samples(min_value,max_value,no_of_samples)
+def get_temperature_in_C():
+    return generate_samples(0, 60, 50)
 
 def celcious_to_farenheit_convertor(temp):
     return round((temp * 1.8) + 32,2)
@@ -10,20 +10,21 @@ def celcious_to_farenheit_convertor(temp):
 def generate_samples(min_value,max_value,no_of_samples):
     return random.sample(range(min_value, max_value),no_of_samples)
   
-def get_charge_rate(min_value,max_value,no_of_samples):
-    return generate_samples(min_value,max_value,no_of_samples)
+def get_charge_rate():
+    return generate_samples(20,80, 50)
 
 def process_data(temp_in_c, charge_rate):
     data= {}
-    data.update({"Temperature" : temp_in_c})
+    data.update({"Temperature" : celcious_to_farenheit_convertor(temp_in_c)})
     data.update({"Charge Rate" : charge_rate})
-   
     return json.dumps(data)
+ 
   
-def display_readings(message):
-    return message
+def stream_data(number):
+    for i in range(number):
+        print(pre_process(get_temperature_in_C(), get_charge_rate()))
+    return "No of Streams Completed = {}".format(number)
   
-def formate_message(attribute,readings):
-    return "The {} readings- {}".format(attribute,readings)  
-  
-  
+if __name__ == '__main__':
+    stream_data(50)
+    
